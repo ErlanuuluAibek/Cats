6c215624ce48dc1001a73ac9304e7e792d3774c2
id251 <danyalider178@gmail.com>
2023-02-15 19:09:58 +0300

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class Cat {
+    private int age;
+    private String name;
+    private ArrayList<Cat> cats=new ArrayList<>();
+
+    public Cat(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Cat() {
+
+    }
+
+    public void Cat(Cat cat){
+        cats.add(cat);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cat cat(int age){
+        for (Cat cat:cats) {
+            if(cat.getAge()==age){
+                return cat;
+            }
+        }
+        return null;
+    }
+    public   Cat cat(String name){
+        for (Cat cat:cats) {
+            if(cat.getName().equals(name)){
+                return cat;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat " +
+                " age: " + age +
+                " name: " + name ;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat=new Cat(2,"tich2");
+        Cat cat2=new Cat(3,"tich3");
+        Cat cat3=new Cat(4,"tich4");
+        Cat cat4=new Cat(1,"tich1");
+        cat.Cat(cat);
+        cat.Cat(cat2);
+        cat.Cat(cat3);
+        cat.Cat(cat4);
+        System.out.println(cat.cat(2));
+        System.out.println(cat.cat("tich3"));
+    }
+}
